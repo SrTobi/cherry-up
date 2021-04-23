8ab0829d5787863f29dfc1a3b2c8120c54ea65ee
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-04-23 17:19:28 +0200

    use curl instead of wget

--- run-lastest-cherry-up.sh
+++ run-lastest-cherry-up.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 tempCherryUp=$(mktemp)
-wget -O $tempCherryUp https://github.com/SrTobi/cherry-up/releases/latest/download/cherry-up.jar
+curl -L -o $tempCherryUp https://github.com/SrTobi/cherry-up/releases/latest/download/cherry-up.jar
 java -jar $tempCherryUp && rm $tempCherryUp &
 disown
